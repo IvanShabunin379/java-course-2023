6e7da4680944417237a00f364ff87e800d233c80
IvanShabunin379 <IvanShabunin379@yandex.ru>
2023-12-26 00:01:07 +0300

    fix

--- src/test/java/edu/hw10/task2/callcache/CallCache.java
+++
@@ -1,9 +0,0 @@
-package edu.hw10.task2.callcache;
-
-import java.io.IOException;
-
-public interface CallCache {
-    boolean put(Object[] args, Object result) throws IOException;
-
-    Object get(Object[] args);
-}
